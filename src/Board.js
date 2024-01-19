a0e4728f0afd2551712804b48dae2d03327bafc8
DD <ddonegal1@gmail.com>
2024-01-19 13:31:26 -0500

    completed part 1

--- src/Board.js
+++ src/Board.js
@@ -27,18 +27,22 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 6, ncols = 6 , chanceLightStartsOn = 0.25 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
-    let initialBoard = [];
+    let initialBoard = Array.from({ length: nrows }, () => Array.from({ length: ncols }, () => Math.random() < chanceLightStartsOn));
     // TODO: create array-of-arrays of true/false values
     return initialBoard;
   }
 
+  
+
   function hasWon() {
     // TODO: check the board in state to determine whether the player has won.
+    return board.every(row => row.every(cell => !cell));
+
   }
 
   function flipCellsAround(coord) {
@@ -54,20 +58,45 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
       };
 
       // TODO: Make a (deep) copy of the oldBoard
+      const newBoard = oldBoard.map(row => [...row]);
 
       // TODO: in the copy, flip this cell and the cells around it
+      flipCell(y,x,newBoard);
+      flipCell(y-1,x, newBoard);
+      flipCell(y+1,x,newBoard);
+      flipCell(y,x-1,newBoard);
+      flipCell(y,x+1,newBoard);
 
       // TODO: return the copy
+      return newBoard;
     });
   }
 
-  // if the game is won, just show a winning msg & render nothing else
+  if(hasWon()) {
+    return <div>You Won!</div>;
+  }
 
-  // TODO
+  const boardRows = board.map((row, rowIndex) =>
+     row.map((isLit, colIndex) => (
+     <Cell
+      key={colIndex} 
+      flipCellsAroundMe={() => flipCellsAround(`${rowIndex}-${colIndex}`)}
+      isLit={isLit}
+    />
+    ))
+  );
 
+ 
   // make table board
-
+return(
+  <table>
+    <tbody>
+      {boardRows.map((row, rowIndex) => <tr key={rowIndex}>{row}</tr>)}
+    </tbody>
+  </table>
+)
   // TODO
+
 }
 
 export default Board;
